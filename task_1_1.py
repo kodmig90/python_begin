b6ef07e0bcd9795764827ab985ff2ecd7e7e2b4c
kodmig90 <kodmig90@mail.ru>
2022-01-19 22:19:50 +0500

    Написано первое задание

--- task_1_1.py
+++ task_1_1.py
@@ -1,1 +1,19 @@
-1
+def convert_time(duration: int) -> str:
+    # пишите реализацию своей программы здесь
+    sec = duration
+    min = sec / 60
+    hour = sec / 3600
+    day = sec / 86400
+    if duration <= 60:
+        return f'{sec} сек'
+    elif 60 <= duration <= 3600:
+        return f'{min} мин {sec % 60} сек'
+    elif 3600 <= duration <= 86400:
+        return f'{hour} час {min % 60} мин {sec % 60} сек'
+    else:
+        return f'{day} дн {hour % 24} час {min % 60} мин {sec % 60} сек'
+
+
+duration = 400153
+result = convert_time(duration)
+print(result)
